fa97af30a9295224b865885b0a78ef50ab66edf3
MikeRusea <162775611+MikeRusea@users.noreply.github.com>
2025-03-23 22:54:23 +0900

    “varidation”

--- src/main/java/com/example/sample1app/repositories/PersonRepository.java
+++ src/main/java/com/example/sample1app/repositories/PersonRepository.java
@@ -2,7 +2,7 @@ package com.example.sample1app.repositories;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepositogit checkoutry;
 import org.springframework.stereotype.Repository;
 
 import com.example.sample1app.Person;
